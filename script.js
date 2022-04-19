091182eb219098ae5f3194a8d4afd1079a446f57
Thomaz <thomaz1593@gmail.com>
2022-04-18 23:26:18 -0300

    all basic functions working

--- script.js
+++ script.js
@@ -1,62 +1,85 @@
-let displayValue;
-let firstNumber;
+const calculator = document.querySelector('.calculator');
 const btnDigit = document.querySelectorAll('.btn-digit');
 const btnOperator = document.querySelectorAll('.btn-operator');
-// const btnAdd = document.querySelector('.btn-add');
-// const btnSub = document.querySelector('.btn-sub');
-// const btnMul = document.querySelector('.btn-mul');
-// const btnDiv = document.querySelector('btn-div');
 const btnEquals = document.querySelector('.btn-equals');
-const outputNumber = document.querySelector('.number');
+const outputNumber = document.querySelector('.display-number');
 
-btnDigit.forEach((button => {
-    button.addEventListener('click', displayDigits);
-}));
+function displayAndStoreKeys(button) {
+    if (!button.target.closest('button')) return;
+    const key = button.target;
+    const keyValue = key.value;
+    const displayValue = outputNumber.textContent;
+    const type = key.dataset.type;
+    // CHECKS IF IT'S A DIGIT BUTTON
+    if (type === 'number') {
+        if (displayValue === '0') {
+            outputNumber.textContent = keyValue;
+        } else if (calculator.dataset.previousKeyType === 'operator') {
+            outputNumber.textContent = keyValue;
+        } else {
+            outputNumber.textContent = displayValue + keyValue;
+        }
+    }
+    // CHECKS IF IT'S A OPERATOR BUTTON
+    if (type === 'operator') {
+        const operatorKeys = btnOperator;
+        operatorKeys.forEach(el => {el.dataset.state = ''})
+        key.dataset.state = 'selected';
 
-function displayDigits(button) {
-    let valButton = this.value;
-    outputNumber.textContent = valButton;
-    return valButton;
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = keyValue;
+    }
+
+    // CHECKS IF IT'S EQUAL BUTTON
+    if (type === 'equals') {
+        const firstNumber = calculator.dataset.firstNumber;
+        const operator = calculator.dataset.operator;
+        const secondNumber = displayValue;
+        console.log(firstNumber, operator, secondNumber);
+
+        outputNumber.textContent = operate(firstNumber, operator, secondNumber);
+    } 
+    calculator.dataset.previousKeyType = type;
 }
 
-btnOperator.forEach((button => {
-    button.addEventListener('click', function(e) {
-        let selectOperator = this.value;
-        outputNumber.textContent = selectOperator;
-        return selectOperator;
-    });
+btnDigit.forEach((button => {
+    button.addEventListener('click', displayAndStoreKeys);
 }));
 
-btnEquals.addEventListener('click', function(e) {
-    operate();
-});
+btnOperator.forEach((button => {
+    button.addEventListener('click', displayAndStoreKeys);
+}));
+
+btnEquals.addEventListener('click', displayAndStoreKeys);
 
 function add(num1, num2) {
-    return num1 + num2;
-    // return result;
+    let result = num1 + num2;
+    return result;
 }
 
 function subtract(num1, num2) {
     let result = num1 - num2;
-    // return result;
+    return result;
 }
 
 function multiply(num1, num2) {
     let result = num1 * num2;
-    // return result;
+    return result;
 }
 
 function divide(num1, num2) {
     let result = num1 / num2;
-    // return result;
+    return result;
 }
 
-function operate(operator, num1, num2) {
-    // operator = btnOperator;
-    switch(operator) {
-        case "+":
-            let result = add(num1, num2);
-            outputNumber.textContent = result;
-            break;
-    }
+function operate(num1, operator, num2) {
+    let result = '';
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (operator === 'plus') result = add(num1, num2);
+    if (operator === 'minus') result = subtract(num1, num2);
+    if (operator === 'times') result = multiply(num1, num2);
+    if (operator === 'divide') result = divide(num1, num2);
+    console.log(result);
+    return result;
 }
